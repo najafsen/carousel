78ab0730d09e14a34d2980c7e86ca3993ce5e6e0
Mohsen Najaflou <mohsen.najaflou@gmail.com>
2020-02-04 19:03:51 +0330

    Initialize app structure and configure prettier

---
+++ src/containers/app/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./App.scss";
+import { Main } from "../main/Main";
+
+export const App = () => {
+  return (
+    <div className="app-component">
+      <Main />
+    </div>
+  );
+};
